3985a82669d848245a0048fac0fe49e21ff8aa36
imorakzai <72260607+imorakzai@users.noreply.github.com>
2025-06-03 01:38:50 +0500

    Fix path module import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const path = require(path)
+const path = require('path')
 
 // require("dotenv").config(); // at the top
 // const path = require('path');
